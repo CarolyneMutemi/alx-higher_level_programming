4dc11d429269612a3ebf8b0fbd83f6c452f05303
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-08-12 01:06:38 +0300

    Check

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        sys.stderr.write("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
-        exit(1)
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    match sys.argv[2]:
-        case "+":
-            print(f"{a} + {b} = {add(a, b)}")
-        case "-":
-            print(f"{a} - {b} = {sub(a, b)}")
-        case "*":
-            print(f"{a} * {b} = {mul(a, b)}")
-        case "/":
-            print(f"{a} / {b} = {div(a, b)}")
-        case _:
-            sys.stderr.write("Unknown operator. Available operators: +, -, * \
-                    and /\n")
-            exit(1)
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
