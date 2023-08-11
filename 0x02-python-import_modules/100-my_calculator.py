f4d4105a88a52452b611c998c942c22287090b5e
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-08-12 01:49:59 +0300

    Check

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
+
     if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        sys.stderr.write("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+        exit(1)
+
+    operator = {"+": add, "-": sub, "*": mul, "/": div}
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    match sys.argv[2]:
-        case "+":
-            print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
-        case "-":
-            print("{} {} {} = {}".format(a, sys.argv[2], b, sub(a, b)))
-        case "*":
-            print("{} {} {} = {}".format(a, sys.argv[2], b, mul(a, b)))
-        case "/":
-            print("{} {} {} = {}".format(a, sys.argv[2], b, div(a, b)))
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+
+    if sys.argv[2] not in list(operator.keys()):
+        sys.stderr.write("Unknown operator. Available operators: +, -, * and /\n")
+
+    print("{} {} {} = {}".format(a, sys.argv[2], b, operator[sys.argv[2]](a, b)))
