7029320086285751412db2ce25cd824062ec5a33
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-08-14 13:50:43 +0300

    CHeck

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,10 +6,13 @@ int is_palindrome(listint_t **head)
 	listint_t *p = *head;
 	int len = 0;
 	int index = 0;
+	int mid = 0;
+	int beg = 0;
+	int end = 0;
 	int *array = malloc(sizeof(int));
-	listint_t *pointer = *head;
+	/*listint_t *pointer = *head;
         listint_t *previous = NULL;
-        listint_t *current = NULL;
+        listint_t *current = NULL;*/
 	if (*head == NULL)
 		return (1);
 
@@ -21,25 +24,18 @@ int is_palindrome(listint_t **head)
 		len++;
 		array = realloc(array, sizeof(int) * len+1);
 	}
+	end = len - 1;
 
-	while (pointer != NULL)
+	if (len % 2 == 0)
+		mid = (len/2) - 1;
+	if (len % 2 == 0)
+		mid = (len/2);
+	while (beg <= mid)
 	{
-		current = pointer;
-		pointer = pointer->next;
-		current->next = previous;
-		previous = current;
-		*head = current;
-	}
-	pointer = *head;
-	index = 0;
-
-	while (pointer != NULL)
-        {
-		if (array[index] != pointer->n)
+		if (array[beg] != array[end])
 			return (0);
-                index++;
-                pointer = pointer->next;
-        }
-	free(array);
+		beg++;
+		end--;
+	}
 	return (1);
 }
