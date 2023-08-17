1446c9cca157ad5317bf066e0040e566b154f2e4
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-08-17 03:56:19 +0300

    Data

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """Returns the weighted average of all \
+    integers tuple (<score>, <weight>)"""
+    num = 0
+    den = 0
+    for elem in my_list:
+        num += (elem[0] * elem[1])
+        den += elem[1]
+
+    return num / den
