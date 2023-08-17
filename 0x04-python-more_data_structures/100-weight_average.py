cd999be2dad0022c958fd667095c6fc1b3b0142a
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-08-17 04:11:18 +0300

    Change

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,6 +4,8 @@ def weight_average(my_list=[]):
     integers tuple (<score>, <weight>)"""
     num = 0
     den = 0
+    if len(my_list) == 0:
+        return 0
     for elem in my_list:
         num += (elem[0] * elem[1])
         den += elem[1]
