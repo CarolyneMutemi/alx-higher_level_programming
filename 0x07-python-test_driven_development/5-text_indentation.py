2e1cef03d43e5a6b9fb83b6fd78b0470f88433d4
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-09-06 05:55:22 +0300

    Tests

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Text indentation module.
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+
+    Args:
+        text - the string to be printed.
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    string = ""
+    for k, i in enumerate(text):
+        if i == "." or i == "?" or i == ":":
+            string += "\n\n"
+            continue
+        if i == " " and (text[k - 1] == "." or text[k - 1] == "?" or text[k - 1] == ":"):
+            string.replace(i, "")
+            continue
+        string += i
+    print(string)
