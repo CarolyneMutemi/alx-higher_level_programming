5cc1e8f192928efd1f44c289877cee5fc8fcb2ee
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-09-11 21:40:30 +0300

    Seven and Nine

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Defines Rectangle class.
+"""
+base_geometry = __import__('7-base_geometry')
+
+
+class Rectangle(base_geometry.BaseGeometry):
+    """
+    Class Rectangle - inherits from BaseGeometry.
+    """
+    def __init__(self, width, height):
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
+
+    def area(self):
+        """
+        Returns the area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
