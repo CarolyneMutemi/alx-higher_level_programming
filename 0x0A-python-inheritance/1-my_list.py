1b9c087fa157674e5841d2ecfe14f906866f45a3
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-09-11 17:35:41 +0300

    Zero

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Defines the MyList function.
+"""
+
+class MyList(list):
+    """
+    MyList class - inherits from class.
+    """
+    def print_sorted(self):
+        """
+        Prints the list, but sorted (ascending sort)
+        """
+        my = self.copy()
+        my.sort()
+        print(my)
