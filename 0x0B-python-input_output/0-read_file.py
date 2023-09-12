7c627c24d2d0873a3616281fb2848fed6517e88b
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-09-12 17:20:01 +0300

    Zero

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Has the read_file function.
+"""
+import os
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+    Args:
+        filename: the name of the file to be read.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")
