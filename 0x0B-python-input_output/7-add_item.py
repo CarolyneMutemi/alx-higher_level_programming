948d9349539c6701ac5c646efce06bb67446df09
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-09-12 22:08:00 +0300

    Seven

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,13 +9,6 @@ import os
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-#with open('add_item.json', 'a+') as file:
-#    file.seek(0)
-#    if file.read() == "":
-#        lis = []
-#    else:
-#        lis = list(load_from_json_file('add_item.json'))
-
 if os.path.exists('add_item.json'):
     lis = list(load_from_json_file('add_item.json'))
 else:
