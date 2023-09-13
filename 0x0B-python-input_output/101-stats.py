2e2ea770fbc570ae8c8c6c47829c4a2b3ac87c3c
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-09-13 18:01:09 +0300

    101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,22 +4,23 @@
 A script that reads stdin line by line and computes metrics.
 """
 
-n = 0
-size = 0
-status = []
-try:
-    while True:
-        stdin = input()
-        lis = stdin.split()
-        n += 1
-        size += int(lis[-1])
-        status.append(lis[-2])
-        status = [i for i in status if i]
-        if n % 10 == 0:
-            print(f"File size: {size}")
-            for i in sorted(set(status)):
-                print(f"{i}: {status.count(i)}")
-except KeyboardInterrupt:
-    print(f"File size: {size}")
-    for i in sorted(set(status)):
-        print(f"{i}: {status.count(i)}")
+if __name__ == "__main__":
+    n = 0
+    size = 0
+    status = []
+    try:
+        while True:
+            stdin = input()
+            lis = stdin.split()
+            n += 1
+            size += int(lis[-1])
+            status.append(lis[-2])
+            status = [i for i in status if i]
+            if n % 10 == 0:
+                print(f"File size: {size}")
+                for i in sorted(set(status)):
+                    print(f"{i}: {status.count(i)}")
+    except KeyboardInterrupt:
+        print(f"File size: {size}")
+        for i in sorted(set(status)):
+            print(f"{i}: {status.count(i)}")
