08e3b0ee2db3d6a8daab7033f9e1b65283881ddf
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-10-19 23:07:45 +0300

    SQL

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,6 @@
+-- Lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
+
+-- Listing...
 SELECT score, count(score) as number
 FROM second_table
 GROUP BY score
