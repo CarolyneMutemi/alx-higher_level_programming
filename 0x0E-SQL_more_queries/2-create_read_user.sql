387765fb8b53cf43a85db6b2863b2f3c8607c18b
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-10-20 04:50:00 +0300

    More SQL

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,6 +3,6 @@
 -- Creating Database.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Creating User.
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'User_0d_2_pwd.';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Granting permission.
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
