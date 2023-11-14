818ce6e6f490e80ed8ead0e06e0de4bfb83807e0
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-11-15 02:28:36 +0300

    100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -4,6 +4,9 @@
  */
 
 const arr1 = require('./100-data.js').list;
-const arr2 = arr1.map((x) => x * arr1.indexOf(x));
+let arr2 = [];
+if (arr1.length > 0) {
+  arr2 = arr1.map((x) => x * arr1.indexOf(x));
+}
 console.log(arr1);
 console.log(arr2);
