8b70d0ee5b4d8d7a59a5d9e4851063e8b6fc7ca2
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-11-15 21:34:19 +0300

    101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,8 +5,8 @@ const dict2 = {};
 const dict1Keys = Object.keys(dict1);
 
 for (const elem in dict1Keys) {
-  const k = dict1[dict1Keys[elem]];
-  if (!(k in Object.keys(dict2))) {
+  const k = dict1[dict1Keys[elem]].toString();
+  if (!dict2[k]) {
     dict2[k] = [dict1Keys[elem]];
   } else {
     dict2[k].push(dict1Keys[elem]);
