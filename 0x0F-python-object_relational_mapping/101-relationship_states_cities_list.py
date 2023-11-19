f7692b1427fe006d4872c7ac5a7a8906c77df758
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-11-19 19:23:56 +0300

    101

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,6 +12,7 @@ Usage:-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_city import City
 from relationship_state import State
 
 if __name__ == "__main__":
