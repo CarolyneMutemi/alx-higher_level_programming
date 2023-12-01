52535bf806a1734d7b905f6a4fc3eda5f8674d60
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-12-01 22:35:29 +0300

    2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter,
+and displays the body of the response
+"""
+
+import sys
+from urllib import request, parse
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    params = {'email': email}
+    params = parse.urlencode(params).encode('utf-8')
+    url = request.Request(url, params)
+    with request.urlopen(url) as data:
+        print(data.read())
