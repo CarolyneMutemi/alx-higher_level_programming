04a7394a15a64f1953abe9a0a05586f4cb75de59
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-12-02 12:56:21 +0300

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,7 +5,6 @@ Lists 10 commits of the repo rails by user rails.
 
 import sys
 import requests
-from datetime import datetime
 
 if __name__ == '__main__':
     repo = sys.argv[1]
@@ -14,14 +13,14 @@ if __name__ == '__main__':
     url = f'https://api.github.com/repos/{owner}/{repo}/commits'
     data = requests.get(url)
     lis = sorted(data.json(),
-                 key=lambda x: datetime.strptime(x['commit']['author']['date'],
-                                                 '%Y-%m-%dT%H:%M:%SZ'),
+                 key=lambda x: x['commit']['author']['date'],
                  reverse=True)
 
     count = 1
     for dic in lis:
-        print(f"{dic.get('sha')}: \
-              {dic.get('commit').get('author').get('name')}")
+        print("{}: {}"
+              .format(dic.get('sha'),
+                      dic.get('commit').get('author').get('name')))
         if count == 10:
             break
         count += 1
