af270ba0b33f62951dbcde0f793bdeef79f2623d
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-12-04 03:39:45 +0300

    1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL, and displays the body of the response.
-if [ "$(curl -sI "$1" | grep HTTP | cut -d " " -f 2)" = '200' ];then curl -s "$1";fi
+curl -sL "$1"
