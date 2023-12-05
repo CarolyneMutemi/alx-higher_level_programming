6cc2c4f57413eae00fbd3250d6d4ce37927cfc37
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-12-05 22:57:20 +0300

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -17,13 +17,18 @@ def find_peak(lis):
     mid = len(lis) // 2
     left = mid - 1
     right = mid + 1
+    # check for an instance where there is no peak in the right side and switch to left
+    check = 0
     while left >= 0 and right <= len(lis) - 1:
         if lis[mid] > lis[left] and lis[mid] > lis[right]:
             return lis[mid]
-        if lis[left] >= lis[mid] and lis[mid] > lis[right]:
+        if (lis[left] >= lis[mid] and lis[mid] > lis[right]) or check:
             mid = left
         else:
             mid = right
+            if right == len(lis) - 1 and lis[len(lis) // 2] == lis[right]:
+                mid = left
+                check = 1
         left = mid - 1
         right = mid + 1
     return lis[mid]
