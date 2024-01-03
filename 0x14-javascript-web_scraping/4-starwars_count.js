5a104ec33dc83bcc19bf7e694b982e3a0d39b3a0
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-01-03 21:04:41 +0300

    4. Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+/**
+ * Prints the number of movies where the character “Wedge Antilles” is present.
+ */
+
+const process = require('process');
+const request = require('request');
+
+const url = process.argv[2];
+const peopleUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+let count = 0;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const bod = JSON.parse(body);
+  const movies = bod.results;
+  for (let index = 0; index < movies.length; index++) {
+    const movie = movies[index];
+    if (movie.characters.includes(peopleUrl)) {
+      count++;
+    }
+  }
+  console.log(count);
+});
