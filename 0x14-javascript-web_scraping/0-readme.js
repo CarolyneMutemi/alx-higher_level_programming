180e2ce2ceee734fee9f1162cbb947bbcc2a59c3
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-01-03 18:32:41 +0300

    0. Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/**
+ * Reads and prints the content of a file.
+ */
+
+const process = require('process');
+const fs = require('fs');
+
+fs.readFile(process.argv[2], (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    console.log(data.toString());
+  }
+});
