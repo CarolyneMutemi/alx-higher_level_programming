f585f80f918a6eefc7c535e67fce4f701049e99c
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-01-11 05:56:54 +0300

    102

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -6,13 +6,13 @@ $(document).ready(function () {
   $('INPUT#btn_translate').click(function () {
     const input = $('input#language_code').val();
     $.get('https://hellosalut.stefanbohacek.dev/',
-    {
-      lang: input
-    },
-    function (data, textStatus) {
-      $('INPUT#btn_translate').css('cursor', 'pointer').click(function () {
-        $('div#hello').text(data.hello);
+      {
+        lang: input
+      },
+      function (data, textStatus) {
+        $('INPUT#btn_translate').css('cursor', 'pointer').click(function () {
+          $('div#hello').text(data.hello);
+        });
       });
-    });
-  })
+  });
 });
